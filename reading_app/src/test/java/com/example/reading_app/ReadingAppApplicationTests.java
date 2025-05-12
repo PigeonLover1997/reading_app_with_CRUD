0241d4b5a675d451cf80fb52efe5f82f92b1722a
S.T. <lotusrain96@gmail.com>
2025-05-12 09:38:24 +0900

    first commit

---
+++ reading_app/src/test/java/com/example/reading_app/ReadingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reading_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
